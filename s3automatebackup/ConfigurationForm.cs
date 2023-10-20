89f5c5cc0a0c3ef19aa6a783bc738ae31b9f6cde
Àlex Màrtir <alex@reteed.com>
2023-10-20 21:46:27 +0200

    #1
    Added:
    -Now we store data in order if the application exits from the system tray we keeep them save. Also data is retreived when the application is hidden and you reveal it again.

--- s3automatebackup/ConfigurationForm.cs
+++ s3automatebackup/ConfigurationForm.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using s3automatebackup;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +15,8 @@ namespace S3AutomateBackup
 {
     public partial class ConfigurationForm : Form
     {
+        private SecureFormStorage storage = new();
+
         readonly Dictionary<int, string> Period = new Dictionary<int, string>
         {
             { 1, "Daily" },
@@ -26,6 +30,17 @@ namespace S3AutomateBackup
             periodComboBox.DataSource = new BindingSource(Period, null);
             periodComboBox.DisplayMember = "Value"; // This displays the string part.
             periodComboBox.ValueMember = "Key";     // This is the underlying value, e.g. for when you want to know which int was selected.
+            string loadedData = storage.LoadFormFields();
+            if (loadedData != null)
+            {
+                string[] fieldValues = loadedData.Split(',');
+                serverTextBox.Text = fieldValues[0];
+                accessKeyTextBox.Text = fieldValues[1];
+                secretKeyTextBox.Text = fieldValues[2];
+                bucketNameTextBox.Text = fieldValues[3];
+                backupFolderTextBox.Text = fieldValues[4];
+                periodComboBox.SelectedIndex = Convert.ToInt32(fieldValues[5]) - 1;
+            }
 
         }
 
@@ -33,10 +48,10 @@ namespace S3AutomateBackup
         {
             S3Uploader uploader = new S3Uploader(serverTextBox.Text, accessKeyTextBox.Text, secretKeyTextBox.Text, bucketNameTextBox.Text);
             int selectedKey = ((KeyValuePair<int, string>)periodComboBox.SelectedItem).Key;
-            string selectedValue = ((KeyValuePair<int, string>)periodComboBox.SelectedItem).Value;
             double intervalMilliseconds = GetIntervalFromPeriod(selectedKey);
             BackupManager backupManager = new BackupManager(backupFolderTextBox.Text, uploader, intervalMilliseconds);
-            // Minimize the app.
+            string formFieldsData = $"{serverTextBox.Text},{accessKeyTextBox.Text},{secretKeyTextBox.Text},{bucketNameTextBox.Text},{backupFolderTextBox.Text},{Convert.ToString(selectedKey)}";
+            storage.SaveFormFields(formFieldsData);
             this.Hide();
         }
 
---
+++ s3automatebackup/SecureFormStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s3automatebackup
+{
+    public class SecureFormStorage
+    {
+        private string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "yourApp", "data.txt");
+
+        private byte[] Protect(string stringData)
+        {
+            byte[] userData = Encoding.UTF8.GetBytes(stringData);
+            return ProtectedData.Protect(userData, null, DataProtectionScope.CurrentUser);
+        }
+
+        private string Unprotect(byte[] encryptedData)
+        {
+            byte[] decryptedData = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(decryptedData);
+        }
+
+        public void SaveFormFields(string data)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath)); // Ensure directory exists
+            byte[] encryptedData = Protect(data);
+            File.WriteAllBytes(filePath, encryptedData);
+        }
+
+        public string LoadFormFields()
+        {
+            if (File.Exists(filePath))
+            {
+                byte[] encryptedData = File.ReadAllBytes(filePath);
+                return Unprotect(encryptedData);
+            }
+            return null;
+        }
+    }
+}
