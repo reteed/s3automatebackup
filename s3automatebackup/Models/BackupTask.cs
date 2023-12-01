f67b09e14bc4f90633b8221aa5f20fdb0544a112
Àlex Màrtir Sabater <alex@reteed.com>
2023-12-02 00:01:57 +0100

    Modified:
    -Some errors in Backup Task class..

--- s3automatebackup/Models/BackupTask.cs
+++ s3automatebackup/Models/BackupTask.cs
@@ -9,11 +9,11 @@ namespace s3automatebackup.Models
     public class BackupTask
     {
         public int Id { get; set; } 
-        public required Configuration Configuration { get; set; }
-        public required string BucketName { get; set; }
-        public required string BackupFolder { get; set; }
+        public Configuration Configuration { get; set; }
+        public string BucketName { get; set; }
+        public string BackupFolder { get; set; }
         public DateTime ScheduledTime { get; set; }
         public int PeriodKey { get; set; }
-        public System.Threading.Timer? Timer { get; set; }
+        public System.Threading.Timer Timer { get; set; }
     }
 }
